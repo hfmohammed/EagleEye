d751122920e15523200971274b4da9a230889f45
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-09-01 02:04:26 -0600

    bug

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ supabase: Client = create_client(SUPABASE_PROJECT_URL, SUPABASE_KEY)
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://dasboard-construction-mhbt-qnam88gu3.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": "https://dasboard-construction-mhbt.vercel.app/"}})
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
 
 FRAME_COUNT = 0
