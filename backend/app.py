bd5d319ae6f3f584d86455c29007501092a56cb5
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-09-06 18:45:29 -0600

    fixed camera.jsx

--- backend/app.py
+++ backend/app.py
@@ -161,4 +161,4 @@ def start_server():
     return "Server started", 200
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True)
