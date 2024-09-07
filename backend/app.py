8ea322a33d89892446efbc99c2a6c6be3453d0c8
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-09-06 19:13:04 -0600

    fixing cors

--- backend/app.py
+++ backend/app.py
@@ -148,4 +148,4 @@ def start_server():
     return "Server started", 200
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5001)
