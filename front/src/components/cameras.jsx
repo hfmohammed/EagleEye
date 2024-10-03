b3ac0262271147d907e35a3377ec1b7d72b22ab5
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-10-02 18:33:23 -0600

    k

--- front/src/components/cameras.jsx
+++ front/src/components/cameras.jsx
@@ -1,16 +1,25 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import Camera from "./Camera"; // Assuming the Camera component is in a separate file
 
 const socket = io("http://localhost:5001");
 
 export function Cameras() {
-  const [imageSrc, setImageSrc] = useState("");
+  const [streams, setStreams] = useState({
+    video1: 0,
+    video2: 0,
+    video3: 0,
+    video4: 0
+  });
 
   useEffect(() => {
-    socket.on("video_frame", ({ frame }) => {
-      setImageSrc(`data:image/jpeg;base64,${frame}`);
+    socket.on("video_frame", ({ frame, streamId }) => {
+      setStreams(prevStreams => ({
+        ...prevStreams,
+        [streamId]: `data:image/jpeg;base64,${frame}`
+      }));
     });
 
     return () => {
@@ -21,13 +30,13 @@ export function Cameras() {
   return (
     <section className="video-section cameras-page">
       <div className="video-grid-col">
-        <img className="video" id="video-1" src={imageSrc} alt="Video Stream 1" key="video-1" />
-        <img className="video" id="video-2" src={imageSrc} alt="Video Stream 2" key="video-2" />
+        <Camera streamId="video1" />
+        <img className="video" id="video-2" src={streams.video2} alt="Video Stream 2" />
       </div>
 
       <div className="video-grid-col">
-        <img className="video" id="video-3" src={imageSrc} alt="Video Stream 3" key="video-3" />
-        <img className="video" id="video-4" src={imageSrc} alt="Video Stream 4" key="video-4" />
+        <img className="video" id="video-3" src={streams.video3} alt="Video Stream 3" />
+        <img className="video" id="video-4" src={streams.video4} alt="Video Stream 4" />
       </div>
     </section>
   );
