dce157371dee7766af51940bef5e80d0e906020a
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-10-17 16:27:18 -0600

    a1

--- front/src/components/analytics.jsx
+++ front/src/components/analytics.jsx
@@ -34,7 +34,7 @@ ChartJS.register(
   PointElement
 );
 
-const socket = io("http://localhost:5001", {
+const socket = io("http://18.204.204.103:5001", {
   transports: ['websocket'],
   upgrade: false,
 });
