c5313d92e23e6ff3aed4e7aa6316a05abc48951f
Ubuntu <hf.mohammed21@gmail.com>
2025-06-26 04:48:23 +0000

    added index when updating db

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     host: '::', // Enables listening on IPv6
     port: 5173,
-    allowedHosts: ['eagleeye.ddnsfree.com'],
+    allowedHosts: ['eagleeye.ooguy.com'],
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8000", // Django backend
