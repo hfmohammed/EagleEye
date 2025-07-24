108a2f0d6098a4ef10e8efbe76432d3d6f0cc517
hassanfarooq00 <hassanfarooq00@gmail.com>
2025-07-24 15:48:31 -0600

    enhanced login+signup ui:

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
   build: {
     outDir: '../server/static',  // Output built files into Django static folder
     emptyOutDir: true,
